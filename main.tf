956a762cc227c8efedd83b9621923a30fe1e8540
nareshgandi <66723519+nareshgandi@users.noreply.github.com>
2024-01-15 19:57:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier             = "my-postgres-db"
+  engine                 = "postgres"
+  engine_version         = "15"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  username               = "postgres"
+  password               = "postgres"
+  parameter_group_name   = "default.postgres15"
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+
+  vpc_security_group_ids = ["sg-0c74eb2fad61f0001"]
+  db_subnet_group_name    = "rdsa-postgresql-db-subnet-group"
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
